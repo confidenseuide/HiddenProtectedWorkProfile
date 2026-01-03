fab60092125ae5c8b4cf8e819727a94e1a374bd7
confidenseuide <confidenseuide@gmail.com>
2026-01-03 17:16:56 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends Activity {
     
     try { Thread.sleep(50); } catch (Exception ignored) {}
 
-		}
+		}}
         if (hasWorkProfile()) {
             launchWorkProfileDelayed();
         } else {
