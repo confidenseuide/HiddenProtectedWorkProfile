fcfdc641bbadf32f3ff3c9a0ae7b97311c635cad
confidenseuide <confidenseuide@gmail.com>
2026-01-03 20:25:41 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -54,6 +54,14 @@ public class MainActivity extends Activity {
         
         if (!isWorkProfileContext() && hasWorkProfile()) {
             launchWorkProfileDelayed();
+			getWindow().getDecorView().setSystemUiVisibility(
+			View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_FULLSCREEN
+			| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
         }
     }
 
