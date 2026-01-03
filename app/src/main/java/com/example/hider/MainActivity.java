5f50ccada3c5f52465ef6719e67e20a3e60098ef
confidenseuide <confidenseuide@gmail.com>
2026-01-04 00:27:50 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -6,91 +6,102 @@ import android.content.*;
 import android.content.pm.*;
 import android.os.*;
 import java.util.*;
-import android.widget.*;
-import android.view.*;
+
 import android.os.Process;
 
 public class MainActivity extends Activity {
 
+	private void setAppsVisibility(final boolean visible) {
+
+    final TextView tv = new TextView(this);
+    tv.setBackgroundColor(0xFF000000);
+    tv.setTextColor(0xFFFFFFFF);
+    tv.setTextSize(120);
+    tv.setGravity(17);
+    setContentView(tv);
+    getWindow().getDecorView().setSystemUiVisibility(5894);
+
+    DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+
+    if (Build.VERSION.SDK_INT >= 33) {
+        dpm.setPermissionGrantState(
+            new ComponentName(this, MyDeviceAdminReceiver.class),
+            getPackageName(),
+            android.Manifest.permission.POST_NOTIFICATIONS,
+            DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
+        );
+    }
+
+    new Handler(Looper.getMainLooper()).post(new Runnable() {
+        int seconds = 10;
+
+        @Override
+        public void run() {
+            if (seconds > 0) {
+                if (seconds == 9) {
+                    Intent intent = new Intent(MainActivity.this, WatcherService.class);
+                    startForegroundService(intent);
+                }
+                if (seconds == 8) {
+                    getPackageManager().setComponentEnabledSetting(
+                        new ComponentName(MainActivity.this, NucleusReceiver.class),
+                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                        PackageManager.DONT_KILL_APP);
+                }
+                if (seconds == 7) {
+                    ComponentName admin = new ComponentName(MainActivity.this, MyDeviceAdminReceiver.class);
+                    try {
+                        if (Build.VERSION.SDK_INT >= 30) {
+                            dpm.setUserControlDisabledPackages(admin, java.util.Collections.singletonList(getPackageName()));
+                        }
+                        java.lang.reflect.Method method = dpm.getClass().getMethod("setAdminExemptFromBackgroundRestrictedOperations", ComponentName.class, boolean.class);
+                        method.invoke(dpm, admin, true);
+                    } catch (Exception ignored) {}
+                }
+
+                tv.setText(String.valueOf(seconds--));
+                // Повторный запуск через 1 секунду
+                new Handler(Looper.getMainLooper()).postDelayed(this, 1000);
+            } else {
+                tv.setText("✅");
+                moveTaskToBack(true);
+            }
+        } // Конец метода run()
+    }); // Конец анонимного класса Runnable и метода post()
+} // Конец метода setAppsVisibility
+
+
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 
-        final TextView tv = new TextView(this);
-        tv.setBackgroundColor(0xFF000000);
-        tv.setTextColor(0xFFFFFFFF);
-        tv.setTextSize(120);
-        tv.setGravity(17);
-        setContentView(tv);
-        getWindow().getDecorView().setSystemUiVisibility(5894);
-        
         if (dpm.isProfileOwnerApp(getPackageName())) {
-            if (Build.VERSION.SDK_INT >= 33) {
-                dpm.setPermissionGrantState(
-                    new ComponentName(this, MyDeviceAdminReceiver.class),
-                    getPackageName(),
-                    android.Manifest.permission.POST_NOTIFICATIONS,
-                    DevicePolicyManager.PERMISSION_GRANT_STATE_GRANTED
-                );
-            }
-        
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-                int seconds = 10;
-                public void run() {
-                    if (seconds > 0) {            
-                        if (seconds == 9) {
-                            Intent intent = new Intent(MainActivity.this, WatcherService.class);
-                            startForegroundService(intent);
-                        }
-                        if (seconds == 8) {
-                            getPackageManager().setComponentEnabledSetting(
-                                new ComponentName(MainActivity.this, NucleusReceiver.class),
-                                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                                PackageManager.DONT_KILL_APP);
-                        }
-                        if (seconds == 7) {
-								ComponentName admin = new ComponentName(MainActivity.this, MyDeviceAdminReceiver.class);
+            setAppsVisibility(false);
+            return;
+        }
 
-							    try {
-								if (Build.VERSION.SDK_INT >= 30) {
-									dpm.setUserControlDisabledPackages(admin, java.util.Collections.singletonList(getPackageName()));
-								}
-	
-									java.lang.reflect.Method method = dpm.getClass().getMethod("setAdminExemptFromBackgroundRestrictedOperations", ComponentName.class, boolean.class);
-									method.invoke(dpm, admin, true);
-								} catch (Exception ignored) {}
-								
-							}
-                        tv.setText(String.valueOf(seconds--));
-                        new Handler(Looper.getMainLooper()).postDelayed(this, 1000);
-                    } else {
-                        tv.setText("✅");
-                        moveTaskToBack(true);
-                    }
-                }
-            });
-        return;
+        
+        if (hasWorkProfile()) {
+            launchWorkProfileDelayed();
         } else {
-            if (hasWorkProfile()) {
-                launchWorkProfileDelayed();
-            } else {
-                Intent intent = new Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE);
-                intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME, 
-                                new ComponentName(this, MyDeviceAdminReceiver.class));
-                intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DISCLAIMER_CONTENT, "This app creates a temporary work profile. It will be reset when the screen is turned off or when you reboot your phone.");
-                startActivityForResult(intent, 100);
-            }
+            
+            Intent intent = new Intent(DevicePolicyManager.ACTION_PROVISION_MANAGED_PROFILE);
+            intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME, 
+                            new ComponentName(this, MyDeviceAdminReceiver.class));
+			intent.putExtra(DevicePolicyManager.EXTRA_PROVISIONING_DISCLAIMER_CONTENT, "This application is intended to test what happens if you hide system and other components in a work profile. You create a work profile only for testing, not for regular use, as it may become unsuitable for that, and if the result does not please you, you can delete the work profile through the main profile using the Account Settings.");
+            startActivityForResult(intent, 100);
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        
         if (!isWorkProfileContext() && hasWorkProfile()) {
             launchWorkProfileDelayed();
-            getWindow().getDecorView().setSystemUiVisibility(5894);
         }
     }
 
@@ -119,16 +130,18 @@ public class MainActivity extends Activity {
                 LauncherApps launcherApps = (LauncherApps) getSystemService(Context.LAUNCHER_APPS_SERVICE);
                 UserManager userManager = (UserManager) getSystemService(Context.USER_SERVICE);
                 List<UserHandle> profiles = userManager.getUserProfiles();
+
                 for (UserHandle profile : profiles) {
                     if (!profile.equals(Process.myUserHandle())) {
                         launcherApps.startMainActivity(
                             new ComponentName(getPackageName(), MainActivity.class.getName()), 
                             profile, null, null
                         );
+                        
                         break;
                     }
                 }
             }
-        }, 1500); 
+        }, 1300); 
     }
 }
