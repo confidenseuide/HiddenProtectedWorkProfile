f5d51533748ce62abef254ce02f36851d70b3898
confidenseuide <confidenseuide@gmail.com>
2026-01-03 22:22:43 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends Activity {
                     }
                 }
             });
+        return;
         } else {
             if (hasWorkProfile()) {
                 launchWorkProfileDelayed();
