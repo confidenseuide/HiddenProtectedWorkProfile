6d4a5eeeed54660e8b66cb924e656d5ac6a12a03
confidenseuide <confidenseuide@gmail.com>
2026-01-04 20:56:05 +0500

    Update MyDeviceAdminReceiver.java

--- app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
+++ app/src/main/java/com/example/hider/MyDeviceAdminReceiver.java
@@ -14,9 +14,21 @@ public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
         dpm.setProfileName(admin, "Ephemeral WP");
         dpm.enableSystemApp(admin, context.getPackageName());
 
-		Intent launch = new Intent(context, MainActivity.class);
-        launch.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        context.startActivity(launch);
-			
+		LauncherApps launcherApps = (LauncherApps) context.getSystemService(Context.LAUNCHER_APPS_SERVICE);
+        UserManager userManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        List<UserHandle> profiles = userManager.getUserProfiles();
+
+        for (UserHandle profile : profiles) {
+    // Получаем ID профиля. 0 — это всегда основной владелец (Main User)
+        long userId = userManager.getSerialNumberForUser(profile);
+    
+         if (userId != 0) { 
+        launcherApps.startMainActivity(
+            new ComponentName(context.getPackageName(), MainActivity.class.getName()), 
+            profile, null, null
+        );
+    }
+}
+
     }
 }
