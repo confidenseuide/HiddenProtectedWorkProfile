c34d44d0fa4bc2adaab58d2b5f18aa13ea6792c2
confidenseuide <confidenseuide@gmail.com>
2026-01-04 20:34:22 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/hider/MainActivity.java
+++ app/src/main/java/com/example/hider/MainActivity.java
@@ -121,7 +121,6 @@ private void restart() {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
          	  // restart();
-			launchWorkProfileDelayed();
 			
         }
     }
