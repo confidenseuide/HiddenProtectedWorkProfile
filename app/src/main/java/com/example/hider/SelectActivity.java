db7732c88a9f4abb8273c341828f465f9574f42a
confidenseuide <confidenseuide@gmail.com>
2026-01-08 13:29:57 +0500

    Update SelectActivity.java

--- app/src/main/java/com/example/hider/SelectActivity.java
+++ app/src/main/java/com/example/hider/SelectActivity.java
@@ -35,6 +35,7 @@ public class SelectActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+		dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);		
 		if (!dpm.isProfileOwnerApp(getPackageName())) {
 			Intent intent = new Intent(context, MainActivity.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
