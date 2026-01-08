2b892ad536a4d74f2aca2b88c6d68e43f7b8a57b
confidenseuide <confidenseuide@gmail.com>
2026-01-08 13:56:19 +0500

    Update SelectActivity.java

--- app/src/main/java/com/example/hider/SelectActivity.java
+++ app/src/main/java/com/example/hider/SelectActivity.java
@@ -46,10 +46,6 @@ public class SelectActivity extends Activity {
 			Intent intent = new Intent(this, MainActivity.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(intent);
-			getPackageManager().setComponentEnabledSetting(
-            new ComponentName(SelectActivity.this, SelectActivity.class),
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-            PackageManager.DONT_KILL_APP);
 			finish();
 		}
 		getWindow().getDecorView().setKeepScreenOn(true);
