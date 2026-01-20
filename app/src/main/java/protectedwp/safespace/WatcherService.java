8eeeb9e92e5bd8e1c2cef6e2e0be0700fc77abfd
confidenseuide <confidenseuide@proton.me>
2026-01-20 13:14:44 +0500

    Update WatcherService.java

--- app/src/main/java/protectedwp/safespace/WatcherService.java
+++ app/src/main/java/protectedwp/safespace/WatcherService.java
@@ -1,1 +1,141 @@
+package protectedwp.safespace;
 
+import android.app.*;
+import java.util.*;
+import java.lang.reflect.*;
+import android.app.admin.*;
+import android.content.*;
+import android.content.pm.*;
+import android.os.*;
+import android.hardware.usb.UsbManager;
+
+public class WatcherService extends Service {
+    private static final String CH_ID = "GuardChan";
+    private BroadcastReceiver receiver;
+    private long startTime;
+
+    private void setAppsVisibility(final boolean visible) {
+    final DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+    final ComponentName admin = new ComponentName(this, MyDeviceAdminReceiver.class);
+    final PackageManager pm = getPackageManager();
+
+    if (!dpm.isProfileOwnerApp(getPackageName())) return;
+    //  We get ALL packages in the current profile, including hidden (uninstalled) ones.
+    List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+
+    for (ApplicationInfo app : packages) {
+        String pkg = app.packageName;
+
+        if (pkg.equals(getPackageName())) {continue;}
+        if (!pm.queryIntentServices(new Intent("android.view.InputMethod").setPackage(pkg), 0).isEmpty()) {continue;}
+
+        Intent launcherIntent = new Intent(Intent.ACTION_MAIN, null);
+        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        launcherIntent.setPackage(pkg);
+
+        List<ResolveInfo> activities = pm.queryIntentActivities(launcherIntent, 
+                PackageManager.MATCH_DISABLED_COMPONENTS | PackageManager.MATCH_UNINSTALLED_PACKAGES);
+
+        if (activities != null && !activities.isEmpty()) {
+            try {
+                dpm.setApplicationHidden(admin, pkg, !visible);
+            } catch (Throwable t00) {
+            }
+        }
+    }
+}
+    
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startTime = System.currentTimeMillis();
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (nm != null && nm.getNotificationChannel(CH_ID) == null) {
+            NotificationChannel channel = new NotificationChannel(
+                CH_ID, "Security System", NotificationManager.IMPORTANCE_LOW);
+            nm.createNotificationChannel(channel);
+        }
+
+        
+        Notification notif = new Notification.Builder(this, CH_ID)
+                .setContentTitle("Profile Protected")
+                .setContentText("it will be frozen on screen off and apps will be hidden.")       
+                .setSmallIcon(android.R.drawable.ic_lock_lock)
+                .setOngoing(true)
+                .build();
+
+        if (Build.VERSION.SDK_INT >= 34) {
+            startForeground(1, notif, ServiceInfo.FOREGROUND_SERVICE_TYPE_SYSTEM_EXEMPTED);
+        } else {
+            startForeground(1, notif);
+        }
+
+       if (receiver == null) {
+            receiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    if (System.currentTimeMillis() - startTime < 3000) return;
+                    DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+                    if (intent != null && UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(intent.getAction())) {try {dpm.wipeData(0);} catch (Throwable tex1) {}} 	
+                    if (intent != null && "android.hardware.usb.action.USB_STATE".equals(intent.getAction())) {try {dpm.wipeData(0);} catch (Throwable tex2) {}}
+                    if (intent != null && Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
+                        
+                        if (dpm != null) {
+                            ComponentName admin = new ComponentName(context, MyDeviceAdminReceiver.class);
+                            setAppsVisibility(false);
+                            
+                            try {
+                                int flag = DevicePolicyManager.class.getField("FLAG_EVICT_CREDENTIAL_ENCRYPTION_KEY").getInt(null);
+                                dpm.lockNow(flag);
+                            } catch (Throwable t01) {
+                                try {
+                                dpm.lockNow(1);
+                                } catch (Throwable t08) {}
+                            }
+                            UserManager um = (UserManager) getSystemService(USER_SERVICE);
+                            if (um.isUserUnlocked(android.os.Process.myUserHandle())) {
+                                try {
+                                dpm.lockNow(1);
+                                } catch (Throwable t07) {}
+                            }
+
+                            try {
+                                int userId = android.os.Process.myUserHandle().hashCode();
+                                Object sm = context.getSystemService("storage");
+                                java.lang.reflect.Method lockMethod = sm.getClass().getMethod("lockUserKey", int.class);
+                                lockMethod.invoke(sm, userId);
+                            } catch (Throwable t02) {
+                                
+                            }
+
+                        }
+                    }
+                }
+            };
+
+            IntentFilter filter = new IntentFilter();
+           filter.addAction(Intent.ACTION_SCREEN_OFF);
+           filter.addAction("android.hardware.usb.action.USB_STATE");
+           filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+            if (Build.VERSION.SDK_INT >= 34) {
+                registerReceiver(receiver, filter, Context.RECEIVER_NOT_EXPORTED);
+            } else {
+                registerReceiver(receiver, filter);
+            }
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (receiver != null) {
+            try { unregisterReceiver(receiver); } catch (Exception ignored) {}
+            receiver = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+}
