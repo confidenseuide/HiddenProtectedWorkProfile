f55e72cc03fda395392bce1a33a340b7d3feb50b
confidenseuide <confidenseuide@proton.me>
2026-01-21 09:54:10 +0500

    Update MediaPlayerActivity.java

--- app/src/main/java/protectedwp/safespace/MediaPlayerActivity.java
+++ app/src/main/java/protectedwp/safespace/MediaPlayerActivity.java
@@ -1,17 +1,13 @@
 package protectedwp.safespace;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.media.AudioAttributes;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.SeekBar;
-import android.widget.Toast;
+import android.app.*;
+import android.content.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
 
 public class MediaPlayerActivity extends Activity {
 
