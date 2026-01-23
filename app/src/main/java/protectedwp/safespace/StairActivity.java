841650dcd2e46e0785b69d082e6e924001b000b7
confidenseuide <confidenseuide@proton.me>
2026-01-23 19:15:01 +0500

    Update StairActivity.java

--- app/src/main/java/protectedwp/safespace/StairActivity.java
+++ app/src/main/java/protectedwp/safespace/StairActivity.java
@@ -5,22 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class StairActivity extends Activity {
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     protected void onResume() {
-        super.onResume();
-        
-        // Запускаем Main и вычищаем старые задачи, чтобы не было мусора в стеке
+        super.onResume();        
         Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
         startActivity(intent);
-        
-        // Оставляем без finish(), как договаривались — Stair останется фундаментом в стеке
-    }
+            }
 }
