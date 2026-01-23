f621ade7546de3585fb5d77abafd721883e36bd4
confidenseuide <confidenseuide@proton.me>
2026-01-23 20:26:36 +0500

    Update SetPasswordActivity.java

--- app/src/main/java/protectedwp/safespace/SetPasswordActivity.java
+++ app/src/main/java/protectedwp/safespace/SetPasswordActivity.java
@@ -16,6 +16,11 @@ public class SetPasswordActivity extends Activity {
   @Override
     protected void onResume() {
         super.onResume();        
+         this.createDeviceProtectedStorageContext()
+            .getSharedPreferences("prefs", Context.MODE_PRIVATE)
+            .edit()
+            .putBoolean("isDone", false)
+            .commit();
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
         startActivity(intent);
