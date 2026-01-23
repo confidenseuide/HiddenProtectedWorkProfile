24e9839c498d42fcdf0749d7b2c7279eac01c5b3
confidenseuide <confidenseuide@proton.me>
2026-01-23 19:29:01 +0500

    Update ZeroActivity.java

--- app/src/main/java/protectedwp/safespace/ZeroActivity.java
+++ app/src/main/java/protectedwp/safespace/ZeroActivity.java
@@ -13,5 +13,11 @@ import android.os.Process;
 
 public class ZeroActivity extends Activity {
 
-  
+  @Override
+    protected void onResume() {
+        super.onResume();        
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);        
+        startActivity(intent);
+    }
 }
