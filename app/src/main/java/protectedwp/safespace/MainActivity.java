eebcc8ebe4e5356d1889161f041a5431a0e957f1
confidenseuide <confidenseuide@proton.me>
2026-01-25 02:19:35 +0500

    Update MainActivity.java

--- app/src/main/java/protectedwp/safespace/MainActivity.java
+++ app/src/main/java/protectedwp/safespace/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends Activity {
     tv.setText("Hello! This is ProtectedWorkProfile app.\n" +
             "The app creates work profile that hide work apps and that will be frozen on screen off and that will be destroyed on USB data connection.\n\n" +
             "Just click start -> next -> next ->... to create profile.\n\n" +
-            "When profile created, the app starts autoconfiguration:\n" +
+            "When profile created, the app starts autoconfiguration timer:\n" +
             "1. App starts service and receiver for screen off / reboot listen.\n" +
             "2. App tries to ignore battery optimization and disable package control to prevent stop-signals from system.\n" +
             "3. App adds \"safest\" system browser to profile (with less permissions from ours blacklist).\n" +
@@ -184,6 +184,7 @@ public class MainActivity extends Activity {
 		    "6. App asks you to set profile password to protect data. It is also recommended to set a password for your main phone, not just your profile.\n"+
 			"7. When screen turns off, profile will be frozen and profie apps hidden (except this app)\n"+
 			"8. To unhide apps just click to \"ProtectedWorkProfile\" shortcut, then \"ShowApps&SetUp\" and wait for the timer.\n\n"+
+			"9. App set password failed attempts limit (3)."
 			"Don't use USB data connection, don't charge phone from PC and other phones if you don't want destroy work profile.\nIf you want to use USB for data transfer or debugging (etc.) without destroying profile, just pause work apps or go to Safe Mode.\n\n");
     scroll.addView(tv);
     root.addView(scroll, sParams);
