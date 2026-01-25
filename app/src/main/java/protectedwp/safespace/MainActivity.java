3a47fea7edd2c05e68a40212425d6163d6bea91d
confidenseuide <confidenseuide@proton.me>
2026-01-25 18:49:33 +0500

    Update MainActivity.java

--- app/src/main/java/protectedwp/safespace/MainActivity.java
+++ app/src/main/java/protectedwp/safespace/MainActivity.java
@@ -182,10 +182,10 @@ public class MainActivity extends Activity {
             "3. App adds \"safest\" system browser to profile (with less permissions from ours blacklist).\n" +
             "4. App disables screenshots in profile (safety), allows apps install and accounts management (user freedom).\n" +
             "5. App selects \"safest\" system keyboard and freezes others.\n"+
-		    "6. App asks you to set profile password to protect data. It is also recommended to set a password for your main phone, not just your profile.\n"+
-			"7. When screen turns off, profile will be frozen and profie apps hidden (except this app)\n"+
-			"8. To unhide apps just click to \"ProtectedWorkProfile\" shortcut, then \"ShowApps&SetUp\" and wait for the timer.\n"+
-			"9. And sets requested password type and minimal length (7), and disables trust agents.\n\n"+
+		    "6. When screen turns off, profile will be frozen and profie apps hidden (except this app)\n"+
+			"7. To unhide apps just click to \"ProtectedWorkProfile\" shortcut, then \"ShowApps&SetUp\" and wait for the timer.\n"+
+			"8. And requests to set safe password type and minimal length (7), and disables trust agents.\n"+
+			"9. App asks you to set password for this profile to protect data (it is also recommended to set a password for your main phone, not only for this profile).\n\n"+
 			"Don't use USB data connection, don't charge phone from PC and other phones if you don't want destroy work profile.\nIf you want to use USB for data transfer or debugging (etc.) without destroying profile, just pause work apps.\n\n");
     scroll.addView(tv);
     root.addView(scroll, sParams);
