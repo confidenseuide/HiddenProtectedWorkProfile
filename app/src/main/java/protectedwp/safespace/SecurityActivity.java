d244827e6905e800b4fac711288f081b9186c9fa
confidenseuide <confidenseuide@proton.me>
2026-01-30 19:55:57 +0500

    Update SecurityActivity.java

--- app/src/main/java/protectedwp/safespace/SecurityActivity.java
+++ app/src/main/java/protectedwp/safespace/SecurityActivity.java
@@ -79,7 +79,7 @@ public class SecurityActivity extends Activity {
     private void checkState() {
         if (!bfuPrefs.contains("pass_hash")) {
             isSetupMode = true;
-            instructionText.setText("Set Unlock Password (that starts autoconfiguration timer, which includes shows apps)\n\n");
+            instructionText.setText("Set Unlock Password (that starts autoconfiguration timer, which includes shows apps)\n");
             actionButton.setText("Next");
         } else {
             isSetupMode = false;
@@ -96,7 +96,7 @@ public class SecurityActivity extends Activity {
             String hashed = hashPassword(input);
             if (tempMainHash == null) {
                 tempMainHash = hashed;
-                instructionText.setText("Set Duress Password (that wipes data)\n\n");
+                instructionText.setText("Set Duress Password (that wipes data)\n");
                 actionButton.setText("Finish Setup");
             } else {
                 bfuPrefs.edit()
