65b30da35206ba38d7e5d9edfa3f2eb7f414ce2a
confidenseuide <confidenseuide@proton.me>
2026-01-31 03:43:15 +0500

    Update SecurityActivity.java

--- app/src/main/java/protectedwp/safespace/SecurityActivity.java
+++ app/src/main/java/protectedwp/safespace/SecurityActivity.java
@@ -45,7 +45,6 @@ public class SecurityActivity extends Activity {
         layout.setOrientation(LinearLayout.VERTICAL);
         int top = (int) (getResources().getDisplayMetrics().heightPixels * 0.30);
         layout.setPadding(60, top, 60, 60);
-        layout.setBackgroundColor(Color.WHITE);
         layout.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
 
         instructionText = new TextView(this);
